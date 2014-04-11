761c8f6d6d675c2a4b199cf56e569e9ceafd7301
Olena Malyshko <olena.malyshko@haw-hamburg.de>
2014-04-11 15:37:48 +0200

    Hast du Abitur Fragen implementiert

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public class Studienberatung{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String answer = scanner.nextLine();
 	}
 }
